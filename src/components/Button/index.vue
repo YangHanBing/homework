c1c9ec016cab108a4ddc28f8297597195450198a
YangHanBing <2180244540@qq.com>
2022-07-18 19:43:23 +0800

    feat(button): button组件封装 - type属性生成主题按钮

---
+++ src/components/Button/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <button class="zy-button" :class="[theme]">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    theme() {
+      return this.type ? `zy-button-${this.type}` : ''
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+@import './index.scss';
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <zy-button>默认按钮</zy-button>
+    <zy-button type="primary">成功按钮</zy-button>
+    <zy-button type="danger">危险按钮</zy-button>
+    <zy-button type="warning">警告按钮</zy-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    zyButton: () => import('../components/Button')
+  }
 }
 </script>
